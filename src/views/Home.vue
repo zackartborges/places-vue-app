78b6553d6d97da7e329fcf8a1ef22f58fef2291a
Zack Borges <zackartborges@gmail.com>
2021-03-26 12:04:35 -0400

    git init + some edits

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div>Place: <button</div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Suh Dude!",
+      places: [],
+      newPlaceAdress: "",
+      newPlaceName: "",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
