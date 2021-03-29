f6cd3297fd0a590bd82b94997e4ef5ef8328a11d
Zack Borges <zackartborges@gmail.com>
2021-03-29 10:52:39 -0400

    update and delete method unable to read place.id for some reason

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
     <div>
+      <h1>New Place!</h1>
       Name:
       <input type="text" v-model="newName" />
       Address:
       <input type="text" v-model="newAdress" />
       <button v-on:click="createPlace">Add a Place</button>
     </div>
+    <h1>All Recipes</h1>
     <div v-for="place in places" v-bind:key="place.id">
-      <h2>{{ place.name }}</h2>
+      <h2>
+        {{ place.name }}
+        {{ place.id }}
+      </h2>
       <button v-on:click="showPlace(place)">More Info</button>
     </div>
     <!-- links show action. Dialog is used to display something -->
@@ -24,8 +28,9 @@
           Adress:
           <input type="text" v-model="currentPlace.adress" />
         </p>
-        <button>Close</button>
+        <button>Destroy</button>
         <button v-on:click="updatePlace">Update Place</button>
+        <button v-on:click="destroyPlace">Close</button>
       </form>
     </dialog>
   </div>
@@ -59,7 +64,7 @@ export default {
       console.log("adding PLACE!");
       var params = {
         name: this.newName,
-        place: this.newAdress,
+        adress: this.newAdress,
       };
       axios
         .post("/api/places", params)
@@ -83,6 +88,13 @@ export default {
       };
       axios.patch("/api/places/" + place.id, params).then((response) => {
         console.log("Success!", response.data);
+      });
+    },
+    destroyPlace: function (place) {
+      axios.delete("/api/places/" + place.id).then((response) => {
+        console.log("Sucess!", response.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
       });
     },
   },
